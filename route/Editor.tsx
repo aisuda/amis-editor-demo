d8777e7b7e47c49059165d30408d84f05824ec49
wuduoyi <wuduoyi@baidu.com>
2021-08-21 14:01:44 +0800

    修样式上的小问题

--- route/Editor.tsx
+++ route/Editor.tsx
@@ -44,8 +44,6 @@ export default inject('store')(
         function renderHeader() {
             return (
                 <div className="editor-header clearfix box-shadow bg-dark">
-                    <div className="navbar-brand text-lt font-thin">AMis 编辑器</div>
-
                     <div className="editor-preview">
                         预览{' '}
                         <Switch
