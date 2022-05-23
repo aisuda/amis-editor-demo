007e8446b8ea5436da90c8dacc39922dc3a0f82c
wibetter <365533093@qq.com>
2022-05-23 11:02:49 +0800

    style: 调整style.css位置

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
+import "./scss/style.scss";
 
 export function bootstrap(mountTo: HTMLElement) {
   ReactDom.render(<App />, mountTo);
