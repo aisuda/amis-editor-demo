6f0903a1cc092feee1efcfd0a6d3facfb417e366
wibetter <365533093@qq.com>
2023-03-21 18:54:02 +0800

    fix: 调整线上示例资源路径

--- amis.config.js
+++ amis.config.js
@@ -55,7 +55,7 @@ module.exports = {
     // 用于构建生产环境代码的相关配置信息
     NODE_ENV: 'production',
     assetsRoot: resolve('./demo'), // 打包后的文件绝对路径（物理路径）
-    assetsPublicPath: 'https://aisuda.github.io/amis-widget-demo/demo/', // 设置静态资源的引用路径（根域名+路径）
+    assetsPublicPath: 'https://aisuda.github.io/amis-editor-demo/demo/', // 设置静态资源的引用路径（根域名+路径）
     assetsSubDirectory: '', // 资源引用二级路径
     productionSourceMap: false,
     productionGzip: false,
